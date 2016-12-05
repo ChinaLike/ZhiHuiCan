37b1f6c4b5b28f4b14e1ae07d005a5c3de0ed180
ChinaLike <572919350@qq.com>
2016-12-05 16:44:40 +0800

    1.修改提交参数英文单词错误 2.本次提交后将修改包名

--- app/src/main/java/cn/sczhckg/order/data/network/RetrofitService.java
+++ app/src/main/java/cn/sczhckg/order/data/network/RetrofitService.java
@@ -33,27 +33,27 @@ public interface RetrofitService {
     /**会员登录*/
     @FormUrlEncoded
     @POST("rest/user/login")
-    Call<Bean<UserLoginBean>> login(@Field("parames") String parames);
+    Call<Bean<UserLoginBean>> login(@Field("params") String params);
     /**主页锅底选择*/
     @FormUrlEncoded
     @POST("rest/openTable/openTableData")
-    Call<Bean<MainPagerShow>> potDataShow(@Field("parames") String parames);
+    Call<Bean<MainPagerShow>> potDataShow(@Field("params") String params);
     /**购物车数据提交*/
     @FormUrlEncoded
     @POST("rest/openTable/openTableVerify")
-    Call<Bean<CommonBean>> openTable(@Field("parames") String parames);
+    Call<Bean<CommonBean>> openTable(@Field("params") String params);
     /**消费者选择点菜方式后请求分类列表*/
     @FormUrlEncoded
     @POST("rest/order/claaify")
-    Call<Bean<ClassifyBean>> classify(@Field("parames") String parames);
+    Call<Bean<ClassifyBean>> classify(@Field("params") String params);
     /**菜品信息*/
     @FormUrlEncoded
     @POST("rest/order/dishesList")
-    Call<Bean<List<DishesBean>>> dishes(@Field("parames") String parames);
+    Call<Bean<List<DishesBean>>> dishes(@Field("params") String params);
     /**菜品详情*/
     @FormUrlEncoded
     @POST("rest/order/deatails")
-    Call<Bean<DetailsBean>> dishesDeatails(@Field("parames") String parames);
+    Call<Bean<DetailsBean>> dishesDeatails(@Field("params") String params);
     /**菜品选择好后数据提交*/
 
     @Deprecated
@@ -65,42 +65,42 @@ public interface RetrofitService {
     /**结账清单数据请求*/
     @FormUrlEncoded
     @POST("rest/accounts/accountsList")
-    Call<Bean<SettleAccountsBean>> settleAccountsList(@Field("parames") String parames);
+    Call<Bean<SettleAccountsBean>> settleAccountsList(@Field("params") String params);
     /**团购券验证*/
     @FormUrlEncoded
     @POST("rest/accounts/accountsGroupon")
-    Call<Bean<GrouponBean>> verifyGroup(@Field("parames") String parames);
+    Call<Bean<GrouponBean>> verifyGroup(@Field("params") String params);
     /**结账*/
     @FormUrlEncoded
     @POST("rest/accounts/accountsPay")
-    Call<Bean<QRCodeBean>> pay(@Field("parames") String parames);
+    Call<Bean<QRCodeBean>> pay(@Field("params") String params);
     /**评价请求数据*/
     @FormUrlEncoded
     @POST("rest/accounts/accountsEvaluateShow")
-    Call<Bean<List<EvaluateBean>>> getEvaluate(@Field("parames") String parames);
+    Call<Bean<List<EvaluateBean>>> getEvaluate(@Field("params") String params);
     /**评价发送数据*/
     @FormUrlEncoded
     @POST("rest/accounts/accountsEvaluate")
-    Call<Bean<CommonBean>> postEvaluate(@Field("parames") String parames);
+    Call<Bean<CommonBean>> postEvaluate(@Field("params") String params);
     /**服务呼叫*/
     @FormUrlEncoded
     @POST("rest/sercive")
-    Call<Bean<CommonBean>> service(@Field("parames") String parames);
+    Call<Bean<CommonBean>> service(@Field("params") String params);
     /**申请办理VIP*/
     @FormUrlEncoded
     @POST("rest/user/applyForVip")
-    Call<Bean<CommonBean>> applyForVip(@Field("parames") String parames);
+    Call<Bean<CommonBean>> applyForVip(@Field("params") String params);
     /**刷新购物车菜品*/
     @FormUrlEncoded
     @POST("rest/refresh/cartDishes")
-    Call<Bean<List<DishesBean>>> refreshCartDishes(@Field("parames") String parames);
+    Call<Bean<List<DishesBean>>> refreshCartDishes(@Field("params") String params);
     /**刷新二维码是否验证成功*/
     @FormUrlEncoded
     @POST("rest/refresh/QRCodeVerify")
-    Call<Bean<CommonBean>> refreshQRCodeIsVerify(@Field("parames") String parames);
+    Call<Bean<CommonBean>> refreshQRCodeIsVerify(@Field("params") String params);
     /**优惠类型验证*/
     @FormUrlEncoded
     @POST("rest/accounts/favorableTypeVerify")
-    Call<Bean<SettleAccountsBean>> favorableTypeVerify(@Field("parames") String parames);
+    Call<Bean<SettleAccountsBean>> favorableTypeVerify(@Field("params") String params);
 
 }
